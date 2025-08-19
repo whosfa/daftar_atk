daefa1598ec8d65a453fc01b675c9f8d44805095
fa <irfazahratuzziqriyah@gmail.com>
2025-08-19 14:23:31 +0700

    admin index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,22 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+   
 </head>
 <body>
+
     <div class="container mt-4">
         <div class="d-flex justify-content-between align-items-center mb-4">
-            <h3 class="mb-0">Daftar ATK</h3>
+            <h3 class="mb-0">Admin Panel</h3>
 
             <div class="d-flex align-items-center">
 
-                {{-- Tombol Tambah --}}
-                <a href="{{ route('admin.create') }}" class="btn btn-primary ms-2 me-2">Tambah</a>
-
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-                    <button type="submit" class="btn btn-danger">Logout</button>
+                    <button type="submit" class="btn btn-danger">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+  <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+</svg>
+                    </button>
                 </form>
             </div>
+
+
         </div>
 
         <div class="mb-3">
@@ -29,16 +35,43 @@
             </form>
         </div>
 
-        <!--tabel data-->
+        <div class="mb-3">
+    <a href="{{ route('admin.index') }}" class="btn btn-secondary">Semua</a>
+    @foreach (\App\Models\ATK::getKategoriList() as $kat)
+        <a href="{{ route('admin.filter', $kat) }}" class="btn btn-primary">{{ $kat }}</a>
+    @endforeach
+</div>
 
-        <table class="table">
+
+         <div class="container mt-4">
+        <div class="d-flex justify-content-between align-items-center mb-4">
+            <h5 class="mb-0">Add New ATK</h5>
+
+            <div class="d-flex align-items-center">
+
+                {{-- Tombol Tambah --}}
+                <a href="{{ route('admin.create') }}" class="">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="green" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                </svg>
+                </a>
+            </div>
+        </div>
+      
+         
+       
+        <!--tabel data-->
+        <h6 class="mb-0">All ATK</h6>
+        <p class="text-body-secondary">berikut adalah daftar atk</p>
+        <div class="w-95 mx-auto">
+                <table class="table table-bordered" style="table-layout:fixed;">
             <thead>
-                <tr class="table-danger">
-                    <th>No</th>
-                    <th>Nama</th>
-                    <th>Stok</th>
-                    <th>Kategori</th>
-                    <th>Aksi</th>
+                <tr class="table table-hover table-secondary">
+                    <th style="width:5%;">No</th>
+                    <th style="width:20%;">Nama</th>
+                    <th style="width:10%;">Stok</th>
+                    <th style="width:20%;">Kategori</th>
+                    <th style="width: 10%;">Aksi</th>
                 </tr>
             </thead>
             <tbody>
@@ -49,24 +82,39 @@
                         <td>{{$atk->stok}}</td>
                         <td>{{$atk->kategori}}</td>
                         <td>
-                            {{-- Hapus --}}
-                            <form action="{{ route('admin.destroy', $atk->id) }}" method="POST" class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm"
-                                    onclick="return confirm('Apakah kamu yakin ingin menghapus item ini?')">Hapus</button>
-                            </form>
+
+                              {{-- Show --}}
+                            <a href="{{ route('atk.show', $atk->id) }}"  style="background: none; border: none; padding: 0; cursor: pointer; outline: none; text-decoration: none; margin-right: 3%;">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+                            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+                            </svg>
+                            </a>
 
                             {{-- Edit --}}
-                            <a href="{{ route('admin.edit', $atk->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                     <a href="{{ route('admin.edit', $atk->id) }}" 
+                        style="background: none; border: none; padding: 0; cursor: pointer; outline: none; text-decoration: none; margin-right: 3%;">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="" class="bi bi-pencil-fill" viewBox="0 0 18 18">
+                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                            </svg>
+                        </a>
 
-                            {{-- Show --}}
-                            <a href="{{ route('atk.show', $atk->id) }}" class="btn btn-info btn-sm">Show</a>
+                              {{-- Hapus --}}
+                            <form action="{{ route('admin.destroy', $atk->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Apakah kamu yakin ingin menghapus item ini?')">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" style="background: none; border: none; padding: 0; cursor: pointer;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="red" class="bi bi-trash3-fill" viewBox="0 0 18 18">
+                                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                                </svg>
+                                </button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+        </div>
     </div>
 </body>
 
